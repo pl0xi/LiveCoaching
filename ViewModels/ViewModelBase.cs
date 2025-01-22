e0b3be2d7e6bd1b6b29c5407444a2ae7594b31cf
Benjamin Sommervoll <puixdk@gmail.com>
2025-01-22 03:36:50 +0100

    MVVM Avalonia template

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LiveCoaching.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LiveCoaching.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
