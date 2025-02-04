11c8be51103295da1da69f4748d0f581f6d04036
Benjamin Sommervoll <puixdk@gmail.com>
2025-02-04 01:11:21 +0100

    Changed project template to WEB API with Controllers

--- Web_Application/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Web_Application.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
